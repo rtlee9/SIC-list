a9247641e4cbe47e1d99e182aa08abc7d9e7fac8
Ryan Lee <ryantlee9@gmail.com>
2016-08-15 19:51:30 -0400

    Set wd as relative

--- tests/context.py
+++ tests/context.py
@@ -3,6 +3,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                '/Users/Ryan/Github/SIC-list/scrape_sic')))
+                '../scrape_sic')))
 
 import scrape_sic_sec
