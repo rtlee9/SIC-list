436633687df9baae418e535065616a0506b28a1c
Ryan Lee <ryantlee9@gmail.com>
2016-08-15 02:06:00 -0400

    Expand testing

--- tests/demo.py
+++ tests/demo.py
@@ -1,7 +1,16 @@
+# Import packages
 import os
 import sys
+import pandas as pd
+
 sys.path.insert(0, os.path.abspath(
     '/Users/Ryan/Github/SIC-list/scrape_sic'))
-
 import scrape_sic_sec
-scrape_sic_sec.get_sic_sec('test.csv')
+
+# Test scrape
+test_scrape = scrape_sic_sec.get_sic_sec('test.csv')
+output_read = pd.read_csv('test.csv')
+print len(test_scrape) - len(output_read)
+
+# Clean up
+os.remove('test.csv')
