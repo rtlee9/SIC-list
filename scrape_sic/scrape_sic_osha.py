8378d58b0a36972dc06e0bf3b19d96827cc8a95e
Ryan Lee <ryantlee9@gmail.com>
2016-08-17 00:32:06 -0400

    Edit parent desc lookup

--- scrape_sic/scrape_sic_osha.py
+++ scrape_sic/scrape_sic_osha.py
@@ -6,6 +6,14 @@ ind_group = namedtuple('ind_group', [
     'full_desc', 'parent_desc', 'link'])
 
 
+def clean_desc(full_desc):
+    full_desc_split = full_desc.split(': ')
+    code_split = full_desc_split.split(' ')
+    code = code_split[len(code_split) - 1]
+    code_type = ' '.join(code_split[len(code_split) - 1])
+    return [code, code_type]
+
+
 def get_divisions():
 
     # Setup
@@ -13,7 +21,7 @@ def get_divisions():
     page = urllib2.urlopen(url).read()
     soup = BeautifulSoup(page, 'html5lib')
 
-    # Read table from ULR
+    # Find content
     container = soup.select('div#maincontain')[0]
     master_list = container.find('div').find('ol')
     all_links = master_list.find_all('a')
@@ -25,18 +33,16 @@ def get_divisions():
         full_desc = str(l.contents[0])
         link = l.get('href')
 
-        if i > 0:  # or if is division
-            parent_desc = all_links[i - 1].contents[0]
+        if (i > 0) & (full_desc.split(' ')[0] == 'Major'):
+            prior_desc = str(all_links[i - 1].contents[0])
+            if prior_desc.split(' ')[0] == 'Division':
+                parent_desc = str(all_links[i - 1].contents[0])
+            elif prior_desc.split(' ')[0] == 'Major':
+                parent_desc = divisions[i - 2].parent_desc
+            else:
+                raise ValueError('Unexpected code type')
         else:
             parent_desc = None
-        divisions.extend(ind_group(full_desc, parent_desc, link))
+        divisions.append(ind_group(full_desc, parent_desc, link))
 
     return divisions
-
-
-def clean_desc(full_desc):
-    full_desc_split = full_desc.split(': ')
-    code_split = full_desc_split.split(' ')
-    code = code_split[len(code_split) - 1]
-    code_type = ' '.join(code_split[len(code_split) - 1])
-    return [code, code_type]
