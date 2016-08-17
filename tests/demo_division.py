ac8f1414129d11ee353aa08f6aeedda730eb5836
Ryan Lee <ryantlee9@gmail.com>
2016-08-17 00:33:39 -0400

    Add division scrape demo

---
+++ tests/demo_division.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(
+    'scrape_sic'))
+import scrape_sic_osha as scrape
+
+divisions = scrape.get_divisions()
+
+for d in divisions:
+    print str(d.parent_desc) + d.full_desc 
